f7e1ab3dac066997da5fddb784b596719ce18b97
zahidmiran <zahidmiron599@gmail.com>
2022-04-30 00:31:16 +0600

    heroku added

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 require('dotenv').config()
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
 
+/* 
+db:  inventoriesdb
+collection: stocks
+*/
+
+const uri = `mongodb+srv://${process.env.M_DB_USER}:${process.env.M_DB_PASS}@cluster0.1ktnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+        const inventoryCollection = client.db('inventoriesdb').collection('stocks')
+
+      const query = {};
+        app.get('/inventories', async(req, res)=>{
+        const inventories = await inventoryCollection.find({}).toArray();
+        res.send(inventories)
+        })
+
+    } 
+    finally {
+
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World! I missed the Batch Ifter party Because of This Assignment?? But what allah done is the best for me?? Happy Coding')
