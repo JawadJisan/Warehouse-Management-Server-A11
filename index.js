0ded47ad25861928b8e33b16b60c43267b0cd2aa
zahidmiran <zahidmiron599@gmail.com>
2022-05-02 22:08:40 +0600

    a

--- index.js
+++ index.js
@@ -68,6 +68,37 @@ async function run() {
       res.send({success: 'Inventory Delevered'})
     });
 
+
+    /* JWF functions */
+    function verifyJWT(req, res, next){
+      const authHeader = req.headers.authorization;
+      if(!authHeader){
+        return res.status(401).send({message: 'UnAuthorized Access'});
+      }
+      const token = authHeader.split(' ')[1];
+      jwt.verify(token, process.env.NEW_ACCESS_TOKEN, (err, decoded)=>{
+        if(err){
+          return res.status(403).send({message: 'Forbideen Access'});
+        }
+        console.log('decodec', decoded);
+        req.decoded = decoded;
+        next();
+      })
+
+    }
+
+
+
+    ///* jwt *////
+    app.post('/getToken', async(req, res)=>{
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.NEW_ACCESS_TOKEN,{
+        expiresIn: '1d'
+      });
+      res.send({accessToken});
+    })
+
+
     // delivered using name
     app.post('/deliveredNAME', async (req, res) =>{
       const orderInfo = req.body;
@@ -76,12 +107,18 @@ async function run() {
     });
 
     // get delivered using user name
-    app.get('/getdeliveredNAME', async (req, res)=>{
+    app.get('/getdeliveredNAME', verifyJWT, async (req, res)=>{
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
+     if(email === decodedEmail){
       const query = {email:email};
       const cursor = deleveryCollection.find(query);
       const inventories = await cursor.toArray();
       res.send(inventories)
+     }
+     else{
+       res.status(403).send({message: "Forbidden Access"});
+     }
     })
 
     // get the partners
